ca262fc7f68977e36a8bb4115c2f641a30141ad8
Mati1432 <mateuszsliwinski9@gmail.com>
2022-04-02 18:58:58 +0200

    add admin panel for order and create forms

---
+++ orders/forms.py
@@ -0,0 +1,20 @@
+"""Forms files."""
+
+from django import forms
+
+from orders.models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'address',
+            'postal_code',
+            'city',
+        ]
+
+
