55eb50f1a1ffdb9fcee307f64af23d312bac12f2
Mati1432 <mateuszsliwinski9@gmail.com>
2022-04-05 23:47:58 +0200

    adding an e-mail reminding the user that he is currently subscribed to the newsletter

--- core/utils.py
+++ core/utils.py
@@ -1,11 +1,14 @@
 import json
 
 from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
 from django.template.loader import render_to_string
 
 from core.models import SettingsMail, Mail
 import requests
 from mailerlite import MailerLiteApi
+
+from project import settings
 
 
 def create_and_send_newsletter(mail):  # noqa D103
@@ -57,10 +60,18 @@ def create_and_send_newsletter(mail):  # noqa D103
     api.campaigns.update(campaign[1]['id'], html=html, plain=plain)
 
     url = f"https://api.mailerlite.com/api/v2/campaigns/{campaign[1]['id']}/actions/send"
-    print('po za')
     if not Mail.objects.filter(email=mail).exists():
-        print('if not')
         return requests.request('POST', url, headers=headers)
     else:
-        print('else')
+        send_mail_password(mail)
         raise ValidationError(message='you are currently in our newsletter.')
+
+
+def send_mail_password(email):  # noqa D103
+    send_mail(
+        'Newsleter',
+        'You are currently subscribed to our newsletter',
+        settings.EMAIL_HOST_USER,
+        [email],
+        fail_silently=False,
+    )
